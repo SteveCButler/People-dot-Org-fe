005fdb7ef88aea6ca07f770b53462694202e6193
Steve Butler <steven.butler@hcaheatlhcare.com>
2023-11-14 15:36:47 -0600

    add peopleList component and update people page to display list of people

--- components/PeopleList.js
+++ components/PeopleList.js
@@ -1,5 +1,40 @@
-const PeopleList = () => (
-  <div>PeopleList</div>
-);
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { deletePerson } from '../api/peopleData';
+
+const PeopleList = ({ personObj, admin, onUpdate }) => {
+  const fullName = `${personObj?.firstName} ${personObj?.lastName}`;
+  const deleteAPerson = () => {
+    if (window.confirm(`Do you want to delete ${fullName}?`)) {
+      deletePerson(personObj.id).then(() => onUpdate());
+    }
+  };
+
+  return (
+
+    <tr>
+      <td>{fullName}</td>
+      <td>{personObj?.email}</td>
+      <td>{personObj?.phone}</td>
+      <td> {admin && <Button className="btn-sm btn-secondary ms-5" onClick={deleteAPerson}><FontAwesomeIcon icon={faTrash} /></Button>} </td>
+    </tr>
+
+  );
+};
 
 export default PeopleList;
+
+PeopleList.propTypes = {
+  personObj: PropTypes.shape({
+    id: PropTypes.number,
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    email: PropTypes.string,
+    phone: PropTypes.string,
+  }).isRequired,
+  admin: PropTypes.bool.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+
+};
